054b2be005ac13f65a2573da3b29858eedace251
Yuri Mello <contato@yurimello.com>
2025-04-24 08:52:58 -0300

    ajusts in repository seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,22 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $sql_path = database_path('municipios_estados_BR.sql');
-        DB::unprepared(file_get_contents($sql_path));
+        // Executa arquivo SQL de municípios e estados
+        $municipios_sql_path = database_path('municipios_estados_BR.sql');
+        DB::unprepared(file_get_contents($municipios_sql_path));
 
-        User::create([
-            'name' => 'Admin',
-            'type' => UserTypeEnum::ADMIN,
-            'email' => 'skay_1994@yahoo.com.br',
-            'password' => 'VddE!-Dn@tJIaxl3Pse8'
-        ]);
+        // Executa arquivo SQL para bancos
+        $banks_sql_path = database_path('banks.sql');
+        DB::unprepared(file_get_contents($banks_sql_path));
 
-//        User::create([
-//            'name' => 'Admin',
-//            'type' => UserTypeEnum::ADMIN,
-//            'email' => 'admin@email.com.br',
-//            'password' => 'VddE!-Dn@tJIaxl3Pse8'
-//        ]);
+        // Executa arquivo SQL para formulários
+        $forms_sql_path = database_path('forms.sql');
+        DB::unprepared(file_get_contents($forms_sql_path));
+
+       User::create([
+           'name' => 'Admin',
+           'type' => UserTypeEnum::ADMIN,
+           'email' => 'admin@email.com.br',
+           'password' => 'VddE!-Dn@tJIaxl3Pse8'
+       ]);
 
         User::create([
             'name' => 'Parceiro',
